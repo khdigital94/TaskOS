ff44e99c2dd7b787edbdfa46a758c9dea6540db0
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-06 00:37:35 +0200

    Inserting dummy data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import "./app.css";
-import { appController, Workspace } from "./modules/appController";
+import { appController, Workspace, Task } from "./modules/appController";
 
 document.addEventListener("DOMContentLoaded", () => {
-	const toDos = appController.getToDos();
+	appController.setToDos();
+
+	const workspace1 = new Workspace("Kevins Arbeitsplatz", crypto.randomUUID());
+	appController.addWorkspace(workspace1);
+
+	const task1 = new Task("Aufräumen", crypto.randomUUID(), "Kevins Arbeitsplatz", "Persönlich", "Wohnzimmer putzen, Schlafzimmer saugen", "20.04.2025", "High");
+	const task2 = new Task("Muddi anrufen", crypto.randomUUID(), "Kevins Arbeitsplatz", "Persönlich", "Sonntags-Call nicht vergessen", "21.04.2025", "Medium");
+	const task3 = new Task("Meeting vorbereiten", crypto.randomUUID(), "Kevins Arbeitsplatz", "Arbeit", "Agenda schreiben, Präsentation updaten", "22.04.2025", "High");
+	const task4 = new Task("Flüge buchen", crypto.randomUUID(), "Kevins Arbeitsplatz", "Reisen", "Check günstige Flüge nach Spanien", "25.04.2025", "Low");
+	const task5 = new Task("Projekt-Deadline", crypto.randomUUID(), "Kevins Arbeitsplatz", "Arbeit", "Code finalisieren, Review einholen", "23.04.2025", "High");
+
+	appController.addTask(task1);
+	appController.addTask(task2);
+	appController.addTask(task3);
+	appController.addTask(task4);
+	appController.addTask(task5);
+
+	task2.setDescription("✅ DAS IST EINE NEUE BESCHREIBUNG");
 });
