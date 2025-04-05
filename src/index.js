5703454d9d19f2e3d6fc938a2b51b24f6c45aae4
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-05 19:28:03 +0200

    Cleaning file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import "./app.css";
-import { appController } from "./modules/appController";
+import { appController, Workspace } from "./modules/appController";
 
 document.addEventListener("DOMContentLoaded", () => {
-	let arr = {
-		name: "Default Workplace",
-		categories: {
-			name: "Persönlich",
-			toDos: {
-				name: "Einkaufen gehen",
-				description: "Laborum elit exercitation proident consectetur sint ex dolore.",
-				dueDate: "10.04.2025",
-				priority: "High",
-				checklist: ["Bananen", "Äpfel", "Fensterreiniger", "Thai Curry", "Toasty"],
-			},
-		},
-	};
-
-	appController.setToDos(arr);
 	const toDos = appController.getToDos();
-
-	console.log(toDos);
 });
