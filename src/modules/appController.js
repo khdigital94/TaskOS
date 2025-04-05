89ac40102799b4351350d5be544b6b2fc791f373
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-05 19:26:40 +0200

    Adding appController and class Workspace
    - appController includes an array with the workspaces, get & set
    functions and functions to add or remove a workspace
    
    - class Workspace creates a new workspace with name, a random id and
    categories. Functions like changing name, adding or removing categories
    and updating the workspace in local storage are included

--- src/modules/appController.js
+++ src/modules/appController.js
@@ -1,16 +1,82 @@
 const appController = (() => {
-	const setToDos = (data) => {
-		localStorage.setItem("Aufgaben", JSON.stringify(data));
+	let workspaces = [
+		{
+			name: "Default Workplace",
+			id: crypto.randomUUID(),
+			categories: {
+				name: "Persönlich",
+				toDos: {
+					name: "Einkaufen gehen",
+					category: "Persönlich",
+					description: "Laborum elit exercitation proident consectetur sint ex dolore.",
+					dueDate: "10.04.2025",
+					priority: "High",
+					checklist: ["Bananen", "Äpfel", "Fensterreiniger", "Thai Curry", "Toasty"],
+				},
+			},
+		},
+	];
+
+	const setToDos = (arr = workspaces) => {
+		localStorage.setItem("Aufgaben", JSON.stringify(arr));
 	};
 
 	const getToDos = () => {
 		return JSON.parse(localStorage.getItem("Aufgaben"));
 	};
 
+	const addWorkspace = (newWorkSpace) => {
+		const workspaces = getToDos();
+		workspaces.push(newWorkSpace);
+		setToDos(workspaces);
+	};
+
+	const removeWorkspace = (workspaceName) => {
+		const workspaces = getToDos();
+		const newArr = workspaces.filter((workspace) => workspace.name !== workspaceName);
+		setToDos(newArr);
+	};
+
 	return {
 		setToDos,
 		getToDos,
+		addWorkspace,
+		removeWorkspace,
 	};
 })();
 
-export { appController };
+class Workspace {
+	constructor(name, id) {
+		this.name = name;
+		this.id = id;
+		this.categories = ["Persönlich", "Arbeit", "Reisen", "Unterhaltung"];
+	}
+
+	changeName(newName) {
+		this.name = newName;
+	}
+
+	addCategory(newCategory) {
+		this.categories.push(newCategory);
+		this.updateWorkspace();
+	}
+
+	removeCategory(oldCategory) {
+		this.categories = this.categories.filter((category) => category !== oldCategory);
+		this.updateWorkspace();
+	}
+
+	updateWorkspace() {
+		const workspaces = appController.getToDos();
+		console.log(workspaces);
+		const index = workspaces.findIndex((workspace) => workspace.id == this.id);
+
+		if (index !== -1) {
+			workspaces[index] = this;
+			console.log(this.id);
+			appController.setToDos(workspaces);
+		}
+	}
+}
+
+export { appController, Workspace };
